051c236d26bff3c523cdb0d1621fe13ece8830de
Leonhard Bosch <lbosch@stud.hs-heilbronn.de>
2021-12-13 15:13:26 +0100

    Added basic command line interface

--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,37 @@ import { GifSource } from "./sources/GifSource";
 
 import { config } from './config/config';
 import { WebServer } from "./webinterface/server";
+import { Command } from 'commander';
 
-let panelsX = 6;
-let panelsY = 4;
-let frameWidth = panelsX * 16;
-let frameHeight = panelsY * 16;
+const program = new Command();
+
+// npm start -- -x 16 -y 16
+
+program
+    .option('-sp, --settingsPage', 'Launches a settings webpage')
+    .requiredOption('-x, --panelWidth <panelWidth>', 'Width of a single panel')
+    .requiredOption('-y, --panelHeight <panelHeight>', 'Height of a single panel')
+    .option('-pxc, --panelWidthCount <panelWidthCount>', 'Number of horizontal panels', '1')
+    .option('-pyc, --panelHeightCount <panelHeightCount>', 'Number of vertical panels', '1')
+    .parse();
+
+const options = program.opts();
+
+let totalPanelCount = options.panelWidthCount * options.panelHeightCount;
+let frameWidth = options.panelWidthCount * options.panelWidth;
+let frameHeight = options.panelHeightCount * options.panelHeight;
 
 let opcSinks: OPCSink[] = [];
 
-for (let i = 0; i < panelsX * panelsY; i++) {
-    // opcSinks.push(new OPCSink(16, 16, '192.168.13.63', 7890+i));
-    opcSinks.push(new OPCSink(16, 16, '127.0.0.1', 7890 + i));
+for (let i = 0; i < totalPanelCount; i++) {
+    opcSinks.push(new OPCSink(options.panelWidth, options.panelHeight, '127.0.0.1', 7890 + i));
 }
 
-let modLedConverter = new ModLedConverter(panelsX, panelsY, 16, 16, opcSinks);
+let modLedConverter = new ModLedConverter(options.panelWidthCount, options.panelHeightCount, options.panelWidth, options.panelHeight, opcSinks);
 // let dummySource = new DummySource(frameWidth, frameHeight, (frame) => {modLedConverter.sendFrame(frame)});
 
 
-let file = "tthl.gif"
+let file = "rgb.gif"
 // let file = "C:/Users/Leandro/Nextcloud/Sync/Projekte/_littleBits/pixelFlut/gifFlut/catNail.gif"
 // let file = "C:/Users/Leandro/Nextcloud/Sync/Projekte/_littleBits/pixelFlut/gifFlut/bongo1.gif"
 // let file = "C:/Users/Leandro/Nextcloud/Sync/Projekte/_littleBits/pixelFlut/gifFlut/catPC.gif"
@@ -33,10 +46,10 @@ let file = "tthl.gif"
 // let file = "C:/Users/Leandro/Nextcloud/Sync/Projekte/_littleBits/pixelFlut/gifFlut/rick.gif"
 // let file = "C:/Users/Leandro/Nextcloud/Sync/Projekte/_littleBits/pixelFlut/gifFlut/simpsons.gif"
 // let file = "C:/Users/Leandro/Nextcloud/Sync/Projekte/_littleBits/pixelFlut/gifFlut/Cyber2.png"
-// let gifSource = new GifSource(frameWidth, frameHeight, (frame) => modLedConverter.sendFrame(frame));
-// gifSource.showGif(file);
+let gifSource = new GifSource(frameWidth, frameHeight, (frame) => modLedConverter.sendFrame(frame));
+gifSource.showGif(file);
 
-let artnetSource = new ArtnetSource(frameWidth, frameHeight, (frame) => modLedConverter.sendFrame(frame));
+//let artnetSource = new ArtnetSource(frameWidth, frameHeight, (frame) => modLedConverter.sendFrame(frame));
 
 let server = new WebServer;
 
